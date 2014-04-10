f7e7ac1d09128840ad40e1aba24aab4ba56b2ae4
Prelang Builder <builder@prelang.com>
2014-04-10 14:29:02 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pollsrus::Application.config.secret_key_base = '89a3eade4cdd97dcbe9d3271db7ff2491dc1c9aab071c63bbcc11e196704a58def30d47d2cf93d02fa2f74f380a76eefa92a3dee1b4235f8b0bff7dd225f8007'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pollsrus::Application.config.session_store :cookie_store, key: '_pollsrus_session'
